dc3a29a6b2237db6d85d1add0afb1d6292116170
Adam Lessey <adamlessey@Adams-MacBook-Air.local>
2026-05-08 09:04:46 -0400

    fix

--- dist/src/game/combat.js
+++
@@ -1,3 +0,0 @@
-export function rollCombatDamage(attackerAttack) {
-    return Math.max(1, Math.floor(Math.random() * attackerAttack) + 1);
-}
--- dist/src/game/random.js
+++
@@ -1,6 +0,0 @@
-export function roll(chance = 1) {
-    return Math.random() < chance;
-}
-export function randomFrom(array) {
-    return array.length ? array[Math.floor(Math.random() * array.length)] : null;
-}
