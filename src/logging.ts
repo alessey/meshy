cf4f9327bcd1716b13f31acc02c3f13adcee34d5
Adam Lessey <adamlessey@Adams-MacBook-Air.local>
2026-05-10 22:18:44 -0400

    logging

--- src/logging.ts
+++ src/logging.ts
@@ -1,13 +1,18 @@
 import { USE_LOGGING } from "./config/constants.js";
 
+function getTimestamp(): string {
+  const now = new Date();
+  return now.toTimeString().split(" ")[0] + ":" + now.getMilliseconds().toString().padStart(3, "0");
+}
+
 export function log(...args: unknown[]): void {
   if (USE_LOGGING) {
-    console.log("[MESHY]", ...args);
+    console.log(`${getTimestamp()}\tINFO\t[MESHY]`, ...args);
   }
 }
 
 export function logError(...args: unknown[]): void {
   if (USE_LOGGING) {
-    console.error("[MESHY]", ...args);
+    console.error(`${getTimestamp()}\tERROR\t[MESHY]`, ...args);
   }
 }
